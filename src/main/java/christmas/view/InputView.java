61cf0abad6c1e3878c6c81df2e1cdfe8fa7bd982
Youngwoo Choi <allnstar@naver.com>
2023-11-10 22:52:04 +0900

    feat: add function for reading order

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -8,4 +8,10 @@ public class InputView {
         String input = Console.readLine();
         return Integer.parseInt(input.trim());
     }
+
+    public String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        return input.trim();
+    }
 }
