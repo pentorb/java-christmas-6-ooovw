f47024dd1848dcf4e1adaea996a701a358c20f7a
Youngwoo Choi <allnstar@naver.com>
2023-11-10 18:51:01 +0900

    feat: add function for counting dessert

--- src/main/java/christmas/model/MenuCategory.java
+++ src/main/java/christmas/model/MenuCategory.java
@@ -9,11 +9,19 @@ public class MenuCategory {
     public static List<String> mainDish = new ArrayList<String>();
     public static List<String> dessert = new ArrayList<String>();
     public static List<String> drink = new ArrayList<String>();
+    public static int amountOfDessert = 0;
 
     public static void categorizeMenu() {
         appetizer = Arrays.asList(Menu.MUSHROOM_SOUP.getName(), Menu.TAPAS.getName(), Menu.CAESAR_SALAD.getName());
         mainDish = Arrays.asList(Menu.T_BONE_STEAK.getName(), Menu.BARBEQUE_RIB.getName(), Menu.SEAFOOD_PASTA.getName(), Menu.CHRISTMAS_PASTA.getName());
         dessert = Arrays.asList(Menu.CHOCO_CAKE.getName(), Menu.ICE_CREAM.getName());
         drink = Arrays.asList(Menu.ZERO_COKE.getName(), Menu.RED_WINE.getName(), Menu.CHAMPAGNE.getName());
+    }
+
+    public static void countDessert() {
+        for (int i = 0; i < dessert.size(); i++)
+            if (Order.userOrder.containsKey(dessert.get(i))) {
+                amountOfDessert += Order.userOrder.get(dessert.get(i));
+            }
     }
 }
