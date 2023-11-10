b45db409e29a6a0334f014853c4028e233956115
Youngwoo Choi <allnstar@naver.com>
2023-11-10 16:04:37 +0900

    feat: add function for checking dessert

--- src/main/java/christmas/utils/Utils.java
+++ src/main/java/christmas/utils/Utils.java
@@ -1,5 +1,8 @@
 package christmas.utils;
 
+import christmas.model.MenuCategory;
+import christmas.model.Order;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
@@ -8,5 +11,13 @@ public class Utils {
         LocalDate dateOfVisit = LocalDate.of(2023, 12, date);
         DayOfWeek dayOfVisit = dateOfVisit.getDayOfWeek();
         return dayOfVisit.getValue();
+    }
+
+    public static boolean isDessert() {
+        for (int i = 0; i < Order.orderedMenu.size(); i++) {
+            if (MenuCategory.dessert.contains(Order.orderedMenu.get(i))) {
+                return true;
+            }
+        }
     }
 }
