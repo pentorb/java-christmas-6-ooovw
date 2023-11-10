feb2e518cc776bcbf3a929d6b26fd51de824ed72
Youngwoo Choi <allnstar@naver.com>
2023-11-10 18:37:31 +0900

    refactor: replace ArrayList with HashMap in class Utils

--- src/main/java/christmas/utils/Utils.java
+++ src/main/java/christmas/utils/Utils.java
@@ -5,11 +5,8 @@ import christmas.model.Order;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Utils {
-    public static List<Integer> dessertIndex = new ArrayList<Integer>();
 
     public static int getDayOfWeek(int date) {
         LocalDate dateOfVisit = LocalDate.of(2023, 12, date);
@@ -20,30 +17,21 @@ public class Utils {
     public static boolean isDessert() {
         boolean result = false;
 
-        for (int i = 0; i < Order.userOrder.size(); i++) {
-            if (MenuCategory.dessert.contains(Order.userOrder.get(i))) {
+        for (int i = 0; i < MenuCategory.dessert.size(); i++)
+            if (Order.userOrder.containsKey(MenuCategory.dessert.get(i))) {
                 result = true;
             }
-        }
         return result;
     }
 
     public static boolean isMainDish() {
         boolean result = false;
 
-        for (int i = 0; i < Order.userOrder.size(); i++) {
-            if (MenuCategory.mainDish.contains(Order.userOrder.get(i))) {
+        for (int i = 0; i < MenuCategory.mainDish.size(); i++) {
+            if (Order.userOrder.containsKey(MenuCategory.mainDish.get(i))) {
                 result = true;
             }
         }
         return result;
-    }
-
-    public static void getDessertIndex() {
-        for (int i = 0; i < Order.userOrder.size(); i++) {
-            if (MenuCategory.dessert.contains(Order.userOrder.get(i))) {
-                dessertIndex.add(i);
-            }
-        }
     }
 }
