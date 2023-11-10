cb31ab26ea5ffa695629171bb7096ea49ac8087d
Youngwoo Choi <allnstar@naver.com>
2023-11-10 21:16:12 +0900

    feat: add function for getting price of menu

--- src/main/java/christmas/model/Price.java
+++ src/main/java/christmas/model/Price.java
@@ -2,4 +2,17 @@ package christmas.model;
 
 public class Price {
     public static int amountOfPrice;
+
+    public static int getPriceOfMenu(String menu) {
+        Menu.putNames();
+        int priceOfMenu = 0;
+
+        for (int i = 0; i < Menu.constantOfMenu.size(); i++){
+            if (menu.equals(Menu.constantOfMenu.get(i).getName())) {
+                priceOfMenu = Menu.constantOfMenu.get(i).getPrice();
+            }
+        }
+
+        return priceOfMenu;
+    }
 }
