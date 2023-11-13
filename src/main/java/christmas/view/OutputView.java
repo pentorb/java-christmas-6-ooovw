f17cad050d6e8adddc5abfc792f0d3f3340404fd
Youngwoo Choi <allnstar@naver.com>
2023-11-13 15:11:39 +0900

    feat: add function for printing date

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -115,4 +115,8 @@ public class OutputView {
     public static void printWelcome() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public static void printDate(int date) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", date);
+    }
 }
