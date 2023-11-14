c89930bc2bc4a7c63bee356cab721c695b06c850
Youngwoo Choi <allnstar@naver.com>
2023-11-14 12:23:40 +0900

    feat: add function for checking menu

--- src/main/java/christmas/exception/Validation.java
+++ src/main/java/christmas/exception/Validation.java
@@ -1,5 +1,10 @@
 package christmas.exception;
 
+import christmas.model.Menu;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Validation {
     public static boolean isNumber(String input) {
         try {
@@ -20,5 +25,23 @@ public class Validation {
         if (number < 1 || number > 31) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static boolean isMenu(String input) {
+        List<String> separatedOrder = Arrays.asList(input.split(","));
+        int vaildCount = 0;
+
+        for (int i = 0; i < separatedOrder.size(); i++) {
+            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
+            if (Menu.nameOfMenu.contains(orderInformation.get(0))) {
+                vaildCount += 1;
+            }
+        }
+
+        if (vaildCount == separatedOrder.size()) {
+            return true;
+        }
+
+        return false;
     }
 }
