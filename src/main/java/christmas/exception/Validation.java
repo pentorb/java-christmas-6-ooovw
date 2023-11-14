4f5091177602538de37fa4c3a9b3fbe17bf53b33
Youngwoo Choi <allnstar@naver.com>
2023-11-14 16:25:06 +0900

    refactor: move methods for checking input to Utils

--- src/main/java/christmas/exception/Validation.java
+++ src/main/java/christmas/exception/Validation.java
@@ -1,25 +1,10 @@
 package christmas.exception;
 
-import christmas.model.Menu;
-import christmas.model.Order;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import christmas.utils.Utils;
 
 public class Validation {
-    public static boolean isNumber(String input) {
-        try {
-            int number = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
     public static void validateNumber(String input) {
-        if (!isNumber(input)) {
+        if (!Utils.isNumber(input)) {
             throw new IllegalArgumentException();
         }
     }
@@ -30,98 +15,26 @@ public class Validation {
         }
     }
 
-    public static boolean isMenu(String input) {
-        List<String> separatedOrder = Arrays.asList(input.split(","));
-        int vaildCount = 0;
-
-        for (int i = 0; i < separatedOrder.size(); i++) {
-            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
-            if (Menu.nameOfMenu.contains(orderInformation.get(0))) {
-                vaildCount += 1;
-            }
-        }
-
-        if (vaildCount == separatedOrder.size()) {
-            return true;
-        }
-
-        return false;
-    }
-
     public static void validateMenu(String input) {
-        if (!isMenu(input)) {
+        if (!Utils.isMenu(input)) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static boolean isMenuNumber(String input) {
-        List<String> separatedOrder = Arrays.asList(input.split(","));
-        int vaildCount = 0;
-
-        for (int i = 0; i < separatedOrder.size(); i++) {
-            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
-            String numberOfMenu = orderInformation.get(1);
-
-            if (isNumber(numberOfMenu) && Integer.parseInt(numberOfMenu) >= 1) {
-                vaildCount += 1;
-            }
-        }
-
-        if (vaildCount == separatedOrder.size()) { //메뉴 형식이 예시와 다를 경우 false를 반환하게됨
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isDuplicateMenu(String input) {
-        List<String> separatedOrder = Arrays.asList(input.split(","));
-        List<String> orders = new ArrayList<>();
-
-        for (int i = 0; i < separatedOrder.size(); i++) {
-            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
-                        orders.add(orderInformation.get(0));
-        }
-
-        HashSet<String> uniqueOrder = new HashSet<>(orders);
-
-        if (separatedOrder.size() == uniqueOrder.size()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isMaximum(String input) {
-        List<String> separatedOrder = Arrays.asList(input.split(","));
-        int orders = 0;
-
-        for (int i = 0; i < separatedOrder.size(); i++) {
-            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
-            orders += Integer.parseInt(orderInformation.get(1));
-        }
-
-        if (orders <= 20) {
-            return true;
-        }
-
-        return false;
-    }
-
     public static void validateMenuNumber(String input) {
-        if (!isMenuNumber(input)) {
+        if (!Utils.isMenuNumber(input)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateDuplicateMenu(String input) {
-        if (!isDuplicateMenu(input)) {
+        if (!Utils.isDuplicateMenu(input)) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void validateMaximum(String input) {
-        if (!isMaximum(input)) {
+        if (!Utils.isMaximum(input)) {
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/christmas/utils/Utils.java
+++ src/main/java/christmas/utils/Utils.java
@@ -1,10 +1,15 @@
 package christmas.utils;
 
+import christmas.model.Menu;
 import christmas.model.MenuCategory;
 import christmas.model.Order;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 public class Utils {
 
@@ -12,5 +17,86 @@ public class Utils {
         LocalDate dateOfVisit = LocalDate.of(2023, 12, date);
         DayOfWeek dayOfVisit = dateOfVisit.getDayOfWeek();
         return dayOfVisit.getValue();
+    }
+
+    public static boolean isNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isMenu(String input) {
+        List<String> separatedOrder = Arrays.asList(input.split(","));
+        int vaildCount = 0;
+
+        for (int i = 0; i < separatedOrder.size(); i++) {
+            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
+            if (Menu.nameOfMenu.contains(orderInformation.get(0))) {
+                vaildCount += 1;
+            }
+        }
+
+        if (vaildCount == separatedOrder.size()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isMenuNumber(String input) {
+        List<String> separatedOrder = Arrays.asList(input.split(","));
+        int vaildCount = 0;
+
+        for (int i = 0; i < separatedOrder.size(); i++) {
+            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
+            String numberOfMenu = orderInformation.get(1);
+
+            if (isNumber(numberOfMenu) && Integer.parseInt(numberOfMenu) >= 1) {
+                vaildCount += 1;
+            }
+        }
+
+        if (vaildCount == separatedOrder.size()) { //메뉴 형식이 예시와 다를 경우 false를 반환하게됨
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isDuplicateMenu(String input) {
+        List<String> separatedOrder = Arrays.asList(input.split(","));
+        List<String> orders = new ArrayList<>();
+
+        for (int i = 0; i < separatedOrder.size(); i++) {
+            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
+            orders.add(orderInformation.get(0));
+        }
+
+        HashSet<String> uniqueOrder = new HashSet<>(orders);
+
+        if (separatedOrder.size() == uniqueOrder.size()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isMaximum(String input) {
+        List<String> separatedOrder = Arrays.asList(input.split(","));
+        int orders = 0;
+
+        for (int i = 0; i < separatedOrder.size(); i++) {
+            List<String> orderInformation = Arrays.asList(separatedOrder.get(i).split("-"));
+            orders += Integer.parseInt(orderInformation.get(1));
+        }
+
+        if (orders <= 20) {
+            return true;
+        }
+
+        return false;
     }
 }
